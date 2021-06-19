d520dadb07ccf50a6500e7045d42027b0e95a516
Robert Shoemaker <rob.shoe187@gmail.com>
2021-06-19 12:21:14 -0400

    added questions array with objects for each question

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+
+var questions = [
+    {
+        question: "What can Javascript arrays store?", 
+        answers: ["Numbers and Strings", "Arrays", "Booleans", "All of the Above"],
+        correct: 4
+    
+    },
+    { 
+        question: "Commonly used data types do not include?", 
+        answers: ["Strings", "Booleans", "Alerts", "Numbers"],
+        correct: 3      
+    },
+
+    {
+        question: "An if/else statement is enclosed with?", 
+        answers: ["Quotes", "Curly Brackets", "Parentheses", "Square Brackets"],
+        correct: 2
+    },
+
+    {
+        question: "A useful tool for printing content to the console for debugging is?", 
+        answers: ["JavaScript", "Terminal/Bash", "For Loops", "console.log"],
+        correct: 4
+    }
+];
